0f69f0829f96cfd6323ec7ab45c36734b1625a95
Emmanuel Montoya <yori.technologies@gmail.com>
2025-11-03 16:52:53 -0600

    Improving Itemlist & items feature

--- lib/image_editor_routes.dart
+++ lib/image_editor_routes.dart
@@ -5,7 +5,7 @@ class PostRoutes {
   static final List<GetPage<dynamic>> routes = [
     ///TO ADD MORE PAGES WHEN IMAGE EDITION GOES MORE DETAILED
     // GetPage(
-    //   name: AppRouteConstants.postUpload,
+    //   name: AppRouteConstants.imageEditor,
     //   page: () => const ImageEditorPage(),
     //   transition: Transition.zoom,
     // ),
